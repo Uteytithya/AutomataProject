c65df5a96af1a7288d48c76a7ad9fc48e811b62e
Zeinsss <tithyautey@gmail.com>
2024-06-05 20:40:13 +0700

    Data structure template established

---
+++ dataStructure.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, foo):
+        self.foo = foo
+    def bar():
+        pass
+class GraphNode:
+    def __init__(self, foo):
+        self.foo = foo
+    def bar():
+        pass    
+class Tree:
+    def __init__(self, foo):
+        self.foo = foo
+    def bar():
+        pass
+class Graph:
+    def __init__(self, foo):
+        self.foo = foo
+    def bar():
+        pass
