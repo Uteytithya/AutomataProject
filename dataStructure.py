7028ddb58d2d7a945d97939d1c1eca40bf7f1a4c
Zeinsss <tithyautey@gmail.com>
2024-06-06 00:07:35 +0700

    Graph structure initialize

--- dataStructure.py
+++ dataStructure.py
@@ -9,12 +9,14 @@ class GraphNode:
     def bar():
         pass    
 class Tree:
-    def __init__(self, foo):
-        self.foo = foo
+    def __init__(self, node):
+        self.node = node
     def bar():
         pass
 class Graph:
-    def __init__(self, foo):
-        self.foo = foo
-    def bar():
+    
+    # To set up graph, needs array that size of array = amount of state and each index has a list for its transitions
+    def __init__(self, node, foo):
+        self.node = GraphNode(foo)
+    def bar(): 
         pass
