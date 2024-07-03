930174bb513d1d35917ef027034c6d3081470483
OukSreySor <ouk.sreysor11@gmail.com>
2024-07-03 11:11:02 +0700

    Copy branch Tithya

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 <h1>Finite Automata Software</h1>
 
-To test fa, declare the fa
-
-```
-FA0 = ({state}, {symbol}, {transitions}, {start state}, {final_state})
-```
-
--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 import functions.fa as fa
 import database.database as db
 import dataStructure.state as state
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
 
 def main():
-    # Create the database
-    db.createDatabase()
+    Q = {'q0', 'q1', 'q2'}
+    X = {'a', 'b'}
+    delta = {
+        ('q0', 'a'): 'q1', ('q0', 'b'): 'q0',
+        ('q1', 'a'): 'q1', ('q1', 'b'): 'q2',
+        ('q2', 'a'): 'q2', ('q2', 'b'): 'q2'
+    }
+    q0 = ('q0')
+    F = {'q2'}
 
-    # Create the initial state
-    initialState = state.State()
+    FA1 = fa.FA(Q, X, delta, q0, F)
+    
+    print(FA1.isDFA())
+    # print(FA1.wordGenerator(5))
+    print(FA1.testString('ab'))
+    print(FA1)  
+    # Generate NFA
+    
+    Q = {'q0', 'q1', 'q2', 'q3'}
+    X = {'a', 'b'}
+    delta = {
+        ('q0', 'a'): {'q1'}, ('q0', ''): {'q2'},
+        ('q1', 'b'): {'q2'},
+        ('q2', 'a'): {'q3'},
+        ('q3', 'b'): {'q0'}
+    }
+    q0 = ('q0')
+    F = {'q0'}
 
-    # Create the genetic algorithm
-    geneticAlgorithm = fa.FA(initialState)
+    FA2 = fa.FA(Q, X, delta, q0, F)
+    DFA2 = FA2.convertNFAtoDFA()
+    print(DFA2.isDFA())
+    print(DFA2)
 
-    # Run the genetic algorithm
-    geneticAlgorithm.run()
 
-    # Close the database
-    db.closeDatabase()
+
 if __name__ == "__main__":
     main()
